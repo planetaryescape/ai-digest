8dca460c6886c36ded0e0cd1380ed1a1b6f49373
Bhekani Khumalo <bhekanik@gmail.com>
2025-08-20 10:59:24 +0100

    chore: remove ~3,600 lines of dead code
    - Remove unused design pattern implementations (builders, decorators, validators)
    - Remove unused utility libraries (fp, lazy loading, connection pooling, saga pattern)
    - Remove old test backup file (.old)
    - Total reduction: 3,622 lines of unused code

--- functions/lib/decorators/index.ts
+++
@@ -1,5 +0,0 @@
-export { Cache, type CacheOptions } from "./Cache";
-export { LogExecution } from "./LogExecution";
-export { Retry, type RetryOptions } from "./Retry";
-export { TrackMetrics } from "./TrackMetrics";
-export { Validate, type ValidationOptions, type ValidationRule, ValidationRules } from "./Validate";
